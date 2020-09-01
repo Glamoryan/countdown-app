d35675bf6c2729e8e0b75f15a90029f27a268a88
Adnan Özdemir <xsabrixsabrix@hotmail.com>
2020-09-01 16:55:31 +0300

    countdown calculated and function started

--- js/countdown.js
+++ js/countdown.js
@@ -15,6 +15,41 @@ function checkContents() {
   } else if (eDate.value == "") {
     alert("Event date cannot be empty");
   } else {
-    
+    startCountdown(eName.value, eDate);
   }
+}
+
+function startCountdown(eventName, eventDateElement) {
+  document.getElementById("event-head").innerHTML = eventName;
+  var time = document.getElementById("event-time");
+
+  var h = time.value.split(":")[0];
+  var m = time.value.split(":")[1];
+
+  var millisecond = Math.floor((h*60*60)*1000);
+  millisecond += Math.floor((m*60)*1000);
+
+
+
+  var date = eventDateElement.value.split(".");
+  var count = new Date(date).getTime(); // milisaniye
+  count += millisecond;
+  var x = setInterval(function () {
+    var now = new Date().getTime();
+    var d = count - now;
+
+    var days = Math.floor(d / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((d % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((d % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((d % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if (d <= 0) {
+      clearInterval(x);
+    }
+  }, 1000);
 }
